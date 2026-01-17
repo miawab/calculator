aad3405d90f199482578abe5b9e517574a83c402
Ibrahim awab <179867549+miawab@users.noreply.github.com>
2026-01-17 11:51:26 +0500

    basic calculation no memory storage of prev answer yet

--- javascript.js
+++ javascript.js
@@ -1,5 +1,12 @@
+document.addEventListener("dblclick",(e)=>{e.preventDefault()}) //idk man check this
+
 let text = "";
+let x = 0;
+let y = 0;
+let op;
+let textArray = [];
 const Btns = document.querySelectorAll("button");
+
 Btns.forEach((btn)=>{
     btn.textContent = `${btn.id}`;
     if(btn.id == "back"){
@@ -19,11 +26,47 @@ function displayContent(text){
     else{content.textContent = text;}
 }
 
+function calculate(op,x,y){
+    switch (op) {
+        case "x":
+            return x*y;
+    
+        case "÷":
+            return x/y;
+
+        case "+":
+            return x+y;
+    
+        case "-":
+            return x-y;
+    }
+}
+
 mainBtns.addEventListener("click",(e)=>{
     let target = e.target;
+    if(target.id == "x"|| target.id == "÷"|| target.id == "-"|| target.id == "+"){
+        op = target.id;
+        console.log("saved booyah")
+    }
     if(target.id != "mainBtns"){
         text += target.id;
+        textArray = text.split("")
+        if((textArray.includes(op) && (target.id != op && target.id != "="))){
+            y += target.id;
+            console.log("ye")
+            console.log(y)
+        }
+        else if(target.id!= op && target.id != "="){
+            x += target.id
+            console.log("no")
+            console.log(x)
+        }
         displayContent(text);
+    }
+    if(target.id == "="){
+        x = calculate(op,x,y)
+        displayContent(x);
+        console.log(x);
     }
 });
 
@@ -35,8 +78,12 @@ topBtns.addEventListener("click",(e)=>{
     }
     if(target.id == "AC"){
         text = "";
+        x = 0;
+        y = 0;
+        op = ""
         content.textContent = text;
     }
 });
+
 
 
